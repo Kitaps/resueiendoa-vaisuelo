efab6bb9f5ebf0f9fe8bca0d490a685fd9f43bd6
Kitaps <paliedke@uc.cl>
2019-12-25 14:18:27 -0400

    Auth finished(?)

--- auth.py
+++ auth.py
@@ -1,5 +1,10 @@
 # Request para autentificar la app con la API
 
 import requests
+from auth_data import client_id, response_type, redirect_uri
 
-requests.get("https://accounts.spotify.com/authorize")
+payload = {client_id: client_id, response_type: response_type, redirect_uri: redirect_uri}
+
+r = requests.get("https://accounts.spotify.com/authorize", params=payload)
+
+print(r)
