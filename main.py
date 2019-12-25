7d9c16e5a9c52664553d93294edbf583a0db2714
Kitaps <paliedke@uc.cl>
2019-12-25 13:25:12 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+# En el pauload se pone el diccionario con los parametros
+payload = dict()
+
+# Se hace el request
+r = requests.get('https://api.github.com/events')
+
+print(r.url)
