25768b5af6f07964689347118e946f3beaca3b2b
Kitaps <paliedke@uc.cl>
2019-12-25 14:37:51 -0400

    Request solo le falta scopes

--- auth.py
+++ auth.py
@@ -1,10 +1,13 @@
 # Request para autentificar la app con la API
 
 import requests
-from auth_data import client_id, response_type, redirect_uri
+from auth_data import client_id, redirect_uri
 
-payload = {client_id: client_id, response_type: response_type, redirect_uri: redirect_uri}
+# client_id se puede sacar de
+# https://developer.spotify.com/dashboard/applications
+payload = {client_id: client_id, response_type: "token", redirect_uri: "https://www.google.com", scope=}
 
 r = requests.get("https://accounts.spotify.com/authorize", params=payload)
 
-print(r)
+# Pagina de HTML ...
+print(r.text)
